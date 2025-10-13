b29faddd2952860cdba2993c137d86d982392866
Josias Silva <josias.silva@koin.com.br>
2025-10-13 16:36:22 -0300

    version: 1.0.0-build.11

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KoinPaymentCheckout",
-            url: "https://koinlatam.github.io/koin-checkout-ios/releases/1.0.0-build.10/KoinPaymentCheckout.xcframework.zip",
-            checksum: "7b8cb97dfad24f6cf94eae9c9667876b208d8db056d957e065440d1940623c70"
+            url: "https://koinlatam.github.io/koin-checkout-ios/releases/1.0.0-build.11/KoinPaymentCheckout.xcframework.zip",
+            checksum: "323b9dd87e69146362613d7f12e1fb04e34c234924c4ba2a100d6050bc081645"
         )
     ]
 )
